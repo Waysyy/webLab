161ad72388c0f35d3c362ae1f9110e55ad541153
Waysy <annatkach225@gmail.com>
2022-10-11 20:05:04 +0700

    lab7

---
+++ lab7/Script.js
@@ -0,0 +1,15 @@
+i = 0
+j = 9
+textik = "f"
+textik2 = "F"
+
+console.log("что-то вывела")
+console.log(i + " " + j)
+console.log(i + j)
+console.log(textik2.toLowerCase() + " " + textik.toUpperCase())
+console.log(i + j, i - j, i * j, i / j, Math.pow(j, 2), j % 2)
+//перенос строки
+console.log("строка\nперенос") 
+//сложить две переменных и присвоить это значение новой переменной
+c = i + j
+console.log(c)
