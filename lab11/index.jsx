a78f9c0f72fad17fbc0b6b72c2bed9d7c8df6ef0
Tkach <annatkach225@gmail.com>
2022-11-23 19:53:04 +0700

    add whatever

---
+++ lab11/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {render} from 'react-dom'
+import App from './components/App'
+
+function List (){
+  const numbers = [1, 2, 3, 4, 5];
+  const listItems = numbers.map((number) =>
+    <li>{number}</li>
+  );
+return( <ul>{listItems}</ul>)
+}
+
+render(
+  <List />,
+  document.getElementById('root'));
+
+
+// const domContainer = document.querySelector('#root');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(e(test));
