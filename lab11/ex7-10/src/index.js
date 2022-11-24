7b2d88c2474c093047a2039d67da681363141587
Waysy <annatkach225@gmail.com>
2022-11-25 00:22:19 +0700

    end

---
+++ lab11/ex7-10/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./ex10/App";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <App />
+);
--- lab11/index.js
+++
@@ -1,5 +0,0 @@
-import App from "./components/App.jsx";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(
-  <App />
-  );
